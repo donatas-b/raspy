0e503b7ee8d3158ab8a2d035638e1e63841b50e6
donatas-b <donatas.barkauskas@gmail.com>
2020-01-08 22:15:28 +0200

    continuing

--- BreathingLED.py
+++ BreathingLED.py
@@ -16,18 +16,18 @@ def setup():
 	GPIO.setup(LedPin, GPIO.OUT)   # set LedPin to OUTPUT mode
 	GPIO.output(LedPin, GPIO.LOW)  # make ledPin output LOW level to turn off LED 
 
-	p = GPIO.PWM(LedPin, 500)      # set PWM Frequence to 500Hz
-	p.start(0)                     # set initial Duty Cycle to 0
+	p = GPIO.PWM(LedPin, 1024)      # set PWM Frequence to 500Hz
+	p.start(1)                     # set initial Duty Cycle to 0
 
 def loop():
 	while True:
 		for dc in range(0, 101, 1):   # make the led brighter
 			p.ChangeDutyCycle(dc)     # set dc value as the duty cycle
-			time.sleep(0.01)
+			time.sleep(0.05)
 		time.sleep(1)
 		for dc in range(100, -1, -1): # make the led darker
 			p.ChangeDutyCycle(dc)     # set dc value as the duty cycle
-			time.sleep(0.01)
+			time.sleep(0.05)
 		time.sleep(1)
 
 def destroy():
